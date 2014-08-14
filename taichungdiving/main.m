c27d29e2d3d4f162dd6caace4ff7a4778a252a01
SamuelTeng <apple@Samuel-Teng.local>
2014-08-14 22:06:04 +0800

    Initial Commit

---
+++ taichungdiving/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  taichungdiving
+//
+//  Created by Samuel Teng on 2014/8/14.
+//  Copyright (c) 2014年 SamuelTeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ taichungdivingTests/taichungdivingTests.m
@@ -0,0 +1,34 @@
+//
+//  taichungdivingTests.m
+//  taichungdivingTests
+//
+//  Created by Samuel Teng on 2014/8/14.
+//  Copyright (c) 2014年 SamuelTeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface taichungdivingTests : XCTestCase
+
+@end
+
+@implementation taichungdivingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
